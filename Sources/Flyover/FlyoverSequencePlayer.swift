0862d01ec377999cba268602c1176d2098d47ea7
Konstantin Cherkashin <tcherkashin94@gmail.com>
2023-05-17 20:17:19 -0600

    player leak fixed

--- Sources/Flyover/FlyoverSequencePlayer.swift
+++ Sources/Flyover/FlyoverSequencePlayer.swift
@@ -19,6 +19,7 @@ public struct FlyoverPlaybackSequence {
 final class FlyoverSequencePlayer: ObservableObject {
     @Published var index: Int = 0
     let sequence: FlyoverPlaybackSequence
+    let isPlaying: Bool
 
     private var timer: Timer.TimerPublisher?
     var bag = Set<AnyCancellable>()
@@ -29,27 +30,32 @@ final class FlyoverSequencePlayer: ObservableObject {
 
     init(sequence: FlyoverPlaybackSequence, isPlaying: Bool) {
         self.sequence = sequence
+        self.isPlaying = isPlaying
         if isPlaying {
             restart()
+        } else {
+            pause()
         }
     }
 
     func restart() {
-        timer?.connect().cancel()
+        stop()
         guard currentPoint.configuration.playbackDuration > 0 else { return }
         timer = Timer.publish(every: currentPoint.configuration.playbackDuration, on: .main, in: .common)
         timer?
             .autoconnect()
             .receive(on: DispatchQueue.main)
-            .sink { date in
+            .sink { [weak self] date in
+                guard let self, self.isPlaying else { return }
                 self.index = (self.index + 1) % self.sequence.points.count
                 self.restart()
             }.store(in: &bag)
 
     }
 
-    func pause() {
+    func stop() {
         timer?.connect().cancel()
+        timer = nil
+        bag = []
     }
-
 }
